4f5ae81393720815c84e479121cca57f09f3656b
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-09 19:33:01 +0530

    Initial Version 15.0

---
+++ ArchDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ArchDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
