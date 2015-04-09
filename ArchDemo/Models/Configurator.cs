7f76c4e3360827b6ee8df02b84c3bddf55339478
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-09 19:43:07 +0530

    added class

---
+++ ArchDemo/Models/Configurator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchDemo.Models
+{
+    public class Configurator
+    {
+        //15.1 changes
+    }
+}
