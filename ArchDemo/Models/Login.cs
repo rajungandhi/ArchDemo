1c26cae86ff1d9264b43cb845ef1904ca94ed212
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-09 19:37:31 +0530

    changes in classes

---
+++ ArchDemo/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchDemo.Models
+{
+    public class Login
+    {
+        //15.0 changes
+        public int LoginID { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ ArchDemo/Models/Timesheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchDemo.Models
+{
+    public class Timesheet
+    {
+        public Timesheet()
+        {
+            //15.0 changes
+            public int TimesheetID { get; set; }
+        }
+    }
+}
