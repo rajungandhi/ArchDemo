639d9469a5b2ab5cae5f8b35d0c174deaed156c9
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-09 19:36:41 +0530

    Added classes for timesheet, expense and login

---
+++ ArchDemo/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchDemo.Models
+{
+    public class Expense
+    {
+        public Expense()
+        {
+            //15.0 changes
+            public int ExpenseID { get; set; }
+        }
+    }
+}
